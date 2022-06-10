1f9a74b67395c16cd0a01edb6daeae6a623a8b1b
Your Name <viniciustatuta@gmail.com>
2022-06-09 21:59:58 -0300

    feat: model product

--- src/main/java/com/store/model/Product.java
+++ src/main/java/com/store/model/Product.java
@@ -1,5 +1,84 @@
 package com.store.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "products")
 public class Product {
+
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "amount")
+    private Integer amount;
+    @Column(name = "price")
+    private double price;
+
+    @ManyToOne
+    private Seller seller;
+
+    
+
+    public Product() {
+    }
+
+
+    public Product(String name, Integer amount, double price, Seller seller) {
+        this.name = name;
+        this.amount = amount;
+        this.price = price;
+        this.seller = seller;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Seller getSeller() {
+        return seller;
+    }
+
+    public void setSeller(Seller seller) {
+        this.seller = seller;
+    }
+
 
 }
