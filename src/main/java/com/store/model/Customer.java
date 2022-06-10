7c8d95d6f02e1e7e8ee564dad86ffbe939f5bc3c
Your Name <viniciustatuta@gmail.com>
2022-06-09 21:23:41 -0300

    feat: model customer

---
+++ src/main/java/com/store/model/Customer.java
@@ -0,0 +1,47 @@
+package com.store.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    private String name;
+    private String CPF;
+    
+    public Customer(String name, String cPF) {
+        this.name = name;
+        CPF = cPF;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getname() {
+        return name;
+    }
+
+    public void setname(String name) {
+        this.name = name;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String cPF) {
+        CPF = cPF;
+    }
+
+    
+    
+}
