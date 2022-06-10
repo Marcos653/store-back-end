edd7b10c9d2a2e01ff5325bfd59a374a204d9f5d
Your Name <viniciustatuta@gmail.com>
2022-06-09 21:52:03 -0300

    feat: model seller

--- src/main/java/com/store/model/Seller.java
+++ src/main/java/com/store/model/Seller.java
@@ -1,51 +1,83 @@
 package com.store.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "customers")
-public class Customer {
-
+@Table(name = "sellers")
+public class Seller {
+    
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
     @Column(name = "name")
     private String name;
-    @Column(name = "CPF")
-    private String CPF;
+    @Column(name = "CNPJ")
+    private String CNPJ;
 
 
-    public Customer(Long id, String name, String cPF) {
-        this.id = id;
-        this.name = name;
-        CPF = cPF;
+    @OneToMany(mappedBy = "seller")
+    private List<Product> products = new ArrayList<>();
+
+
+    public Seller() {
     }
+
+
+    public Seller(String name, String cNPJ) {
+        this.name = name;
+        CNPJ = cNPJ;
+    }
+
 
     public Long getId() {
         return id;
     }
+
+
     public void setId(Long id) {
         this.id = id;
     }
+
+
     public String getName() {
         return name;
     }
+
+
     public void setName(String name) {
         this.name = name;
     }
-    public String getCPF() {
-        return CPF;
-    }
-    public void setCPF(String cPF) {
-        CPF = cPF;
+
+
+    public String getCNPJ() {
+        return CNPJ;
     }
 
 
+    public void setCNPJ(String cNPJ) {
+        CNPJ = cNPJ;
+    }
+
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
     
-    
+
+
 }
